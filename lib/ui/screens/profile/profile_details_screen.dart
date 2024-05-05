286c19a193721c6234055e57d788654f1531a0e6
Sri Satya <satya@janaspandana.in>
2024-05-06 00:07:30 +0530

    Add edit profile_details_screen.dart

--- lib/ui/screens/profile/profile_details_screen.dart
+++ lib/ui/screens/profile/profile_details_screen.dart
@@ -20,110 +20,136 @@ class ProfileDetailsScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: MyColors.accentColor,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // const Center(
-                //   child: Avatar(
-                //     size: 70,
-                //     url: '',
-                //     name: '',
-                //     color: Colors.white,
-                //     shape: BoxShape.circle,
-                //     borderRadius: null,
-                //   ),
-                // ),
-                // const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
+          Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: MyColors.accentColor,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // const Center(
+                    //   child: Avatar(
+                    //     size: 70,
+                    //     url: '',
+                    //     name: '',
+                    //     color: Colors.white,
+                    //     shape: BoxShape.circle,
+                    //     borderRadius: null,
+                    //   ),
+                    // ),
+                    // const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Image.asset(Images.name, width: 12, height: 12),
-                          const SizedBox(width: 12),
-                          Text(
-                            (userBloc.profile.name ?? 'NA').toCapitalized(),
-                            style: textTheme.titleSmall!.copyWith(
-                              color: MyColors.accentColor1,
-                            ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(Images.name, width: 12, height: 12),
+                              const SizedBox(width: 12),
+                              Text(
+                                (userBloc.profile.name ?? 'NA').toCapitalized(),
+                                style: textTheme.titleSmall!.copyWith(
+                                  color: MyColors.accentColor1,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(Images.mail, width: 12, height: 12),
+                              const SizedBox(width: 12),
+                              Text(
+                                userBloc.profile.email ?? 'NA',
+                                style: textTheme.titleSmall!.copyWith(
+                                  color: MyColors.accentColor1,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(Images.call, width: 12, height: 12),
+                              const SizedBox(width: 12),
+                              Text(
+                                userBloc.profile.mobile ?? 'NA',
+                                style: textTheme.titleSmall!.copyWith(
+                                  color: MyColors.accentColor1,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(Images.cam, width: 12, height: 12),
+                              const SizedBox(width: 12),
+                              Text(
+                                (userBloc.profile.studio ?? 'NA')
+                                    .toCapitalized(),
+                                style: textTheme.titleSmall!.copyWith(
+                                  color: MyColors.accentColor1,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                Images.address,
+                                width: 12,
+                                height: 12,
+                              ),
+                              const SizedBox(width: 12),
+                              Text(
+                                userBloc.profile.address ?? 'NA',
+                                style: textTheme.titleSmall!.copyWith(
+                                  color: MyColors.accentColor1,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(Images.mail, width: 12, height: 12),
-                          const SizedBox(width: 12),
-                          Text(
-                            userBloc.profile.email ?? 'NA',
-                            style: textTheme.titleSmall!.copyWith(
-                              color: MyColors.accentColor1,
-                            ),
-                          ),
-                        ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 15,
+                right: 15,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      Routes.editProfile.setId(
+                        userBloc.profile.id.toString(),
                       ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(Images.call, width: 12, height: 12),
-                          const SizedBox(width: 12),
-                          Text(
-                            userBloc.profile.mobile ?? 'NA',
-                            style: textTheme.titleSmall!.copyWith(
-                              color: MyColors.accentColor1,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(Images.cam, width: 12, height: 12),
-                          const SizedBox(width: 12),
-                          Text(
-                            (userBloc.profile.studio ?? 'NA').toCapitalized(),
-                            style: textTheme.titleSmall!.copyWith(
-                              color: MyColors.accentColor1,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            Images.address,
-                            width: 12,
-                            height: 12,
-                          ),
-                          const SizedBox(width: 12),
-                          Text(
-                            userBloc.profile.address ?? 'NA',
-                            style: textTheme.titleSmall!.copyWith(
-                              color: MyColors.accentColor1,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    );
+                  },
+                  child: Text(
+                    'Edit',
+                    style: textTheme.titleSmall!.copyWith(
+                      color: MyColors.accentColor1,
+                      fontSize: 11,
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
