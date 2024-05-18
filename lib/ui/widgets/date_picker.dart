2c7d1e5299bec46a6370752a8552765587b0f5ce
Sri Satya <satya@janaspandana.in>
2024-05-19 01:48:03 +0530

    Remove unused widgets & packages

--- lib/ui/widgets/date_picker.dart
+++ lib/ui/widgets/date_picker.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../../resources/images.dart';
+import '../../resources/images.dart';
 import '../../resources/theme.dart';
 
+@immutable
 class DatePicker extends StatelessWidget {
-  DateTime? date;
-  TextEditingController dateCtrl;
-  final Function onDateChange;
+  final DateTime? date;
+  final TextEditingController dateCtrl;
+  final Function? onDateChange;
   final DateTime? startDate;
   final DateTime? endDate;
   final String? hintText;
@@ -15,11 +16,11 @@ class DatePicker extends StatelessWidget {
   final bool? filled;
   final bool validator;
 
-  DatePicker(
+  const DatePicker(
     this.date, {
     Key? key,
     required this.dateCtrl,
-    required this.onDateChange,
+    this.onDateChange,
     this.startDate,
     this.endDate,
     this.hintText,
@@ -34,53 +35,48 @@ class DatePicker extends StatelessWidget {
     return TextFormField(
       readOnly: true,
       controller: dateCtrl,
-      style: textTheme.bodyLarge,
-      onTap: () async {
-        FocusScope.of(context).requestFocus(FocusNode());
-        date = await showDatePicker(
-          context: context,
-          initialDate: startDate ?? date ?? DateTime.now(),
-          firstDate: startDate ?? DateTime(1900),
-          lastDate: endDate ?? DateTime(2100),
-          builder: (context, child) {
-            return Theme(
-              data: Theme.of(context).copyWith(
-                textTheme: AppTheme.theme.textTheme,
-                colorScheme: AppTheme.theme.colorScheme,
-              ),
-              child: child!,
-            );
-          },
-        );
-        dateCtrl.text =
-            date != null ? DateFormat('dd MMMM, yyyy').format(date!) : '';
-        onDateChange.call(date);
-      },
-      validator: validator == true
-          ? (value) {
+      style: textTheme.titleMedium,
+      onTap: onDateChange != null
+          ? () async {
+              FocusScope.of(context).requestFocus(FocusNode());
+              DateTime? dateNow = await showDatePicker(
+                context: context,
+                initialDate: startDate ?? date ?? DateTime.now(),
+                firstDate: startDate ?? DateTime(1900),
+                lastDate: endDate ?? DateTime(2100),
+                builder: (context, child) {
+                  return Theme(
+                    data: Theme.of(context).copyWith(
+                      textTheme: AppTheme.theme.textTheme,
+                      colorScheme: AppTheme.theme.colorScheme,
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              dateCtrl.text = dateNow != null
+                  ? DateFormat('dd MMMM, yyyy').format(dateNow)
+                  : 'Select Date';
+              onDateChange?.call(dateNow);
+            }
+          : null,
+      onSaved: (v) => dateCtrl.text = v!,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText ?? '',
+        suffixIcon: IconButton(
+          onPressed: null,
+          icon: Image.asset(Images.calendar, width: 18, height: 18),
+        ),
+      ),
+      validator: validator == false
+          ? null
+          : (value) {
               if ((value == null || value.trim().isEmpty && value == '')) {
                 return 'This field can\'t be empty';
               }
               return null;
-            }
-          : (v) {
-              return null;
             },
-      onSaved: (v) => dateCtrl.text = v!,
-      autocorrect: true,
-      decoration: InputDecoration(
-        isDense: true,
-        filled: filled ?? false,
-        labelText: labelText,
-        hintText: hintText ?? '',
-        hintStyle: textTheme.titleMedium!.copyWith(
-          color: Colors.black.withOpacity(0.5),
-        ),
-        suffixIcon: IconButton(
-          onPressed: null,
-          icon: Image.asset(Images.calendar, width: 18),
-        ),
-      ),
     );
   }
 }
